96df19567f5c17ed7fc6aae5d880680a545b6b85
Sheng Yang <yangsheng6810@gmail.com>
2020-05-12 17:54:30 -0500

    More functionality for update_git.sh

--- update_git.sh
+++ update_git.sh
@@ -2,12 +2,27 @@
 if [ -z $GIT ]; then
     GIT="git"
 fi
-GIT_URL=$(${GIT} remote get-url origin)
-if [[ $GIT_URL == *"dimlight.ml"* ]]; then
-    GIT_REPO_NAME="${GIT_URL##*/}"
-    echo "Updating $GIT url"
-    NEW_GIT_REPO="ssh://git@racks.dimlight.net:8022/${GIT_REPO_NAME}"
-    echo "from: $GIT_URL"
-    echo "to: $NEW_GIT_REPO"
-    ${GIT} remote set-url origin ${NEW_GIT_REPO}
+function update_git {
+    GIT_URL=$(${GIT} remote get-url origin)
+    if [[ $GIT_URL == *"dimlight"* ]]; then
+        GIT_REPO_NAME="${GIT_URL##*/}"
+        GIT_REPO_NAME="${GIT_REPO_NAME##*:}"
+        echo "Updating $GIT url"
+        NEW_GIT_REPO="ssh://git@racks.dimlight.net:8022/${GIT_REPO_NAME}"
+        echo "from: $GIT_URL"
+        echo "to: $NEW_GIT_REPO"
+        ${GIT} remote set-url origin ${NEW_GIT_REPO}
+    fi
+}
+if [[ -z $1 ]]; then
+    update_git
+else
+    DIR=$1
+    DIR=${DIR%"/.git"}
+    echo $DIR
+    pushd .
+    cd $DIR
+    pwd
+    update_git
+    popd
 fi
