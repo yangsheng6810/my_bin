6012481f4347de1414137ce7ae24000c3b2347f9
Sheng Yang <yangsheng6810@gmail.com>
2021-04-29 03:10:30 -0500

    Update convert_filename.sh

--- convert_filename.sh
+++ convert_filename.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-FROM_ENCODING="euc-jp"
+FROM_ENCODING="gb2312"
+# FROM_ENCODING="euc-jp"
 # FROM_ENCODING="shift_jis"
-convmv -f utf8 -t iso88591 *.cue --notest --nosmart && convmv -f $FROM_ENCODING -t utf8 *.cue --notest --nosmart;
+convmv -f utf8 -t iso88591 *.xlsx --notest --nosmart && convmv -f $FROM_ENCODING -t utf8 *.xlsx --notest --nosmart;
