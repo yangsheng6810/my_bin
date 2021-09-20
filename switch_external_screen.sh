9a68df8ea18f1f56cb281792824084c725148967
Sheng Yang <yangsheng6810@gmail.com>
2021-09-20 16:30:42 -0500

    Add script to switch external monitor on/off

---
+++ switch_external_screen.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+NUM_MONITOR=$(xrandr | grep '*' | wc -l)
+echo $NUM_MONITOR
+
+if [[ "$NUM_MONITOR" == 2 ]]; then
+    ssh carbon 'DISPLAY=":0" xrandr --output eDP1 --scale 1x1 --mode 2560x1440 --rotate normal'
+    ssh carbon 'DISPLAY=":0" xset -dpms s off'
+    killall x11vnc
+    xrandr --output HDMI-1 --off --output HDMI-0 --mode "2560x1440" --pos 0x0
+else
+    ssh carbon 'DISPLAY=":0" xrandr --output eDP1 --scale 1x1 --mode 1600x900 --rotate right'
+    ssh carbon 'DISPLAY=":0" xset +dpms s 1200 1200'
+    xrandr --output HDMI-1 --mode "1600x900" --pos 2560x180 --rotate left --output HDMI-0 --mode "2560x1440" --pos 0x0
+    x11vnc -usepw -nocursorshape -nocursorpos -nobell -noxinerama -solid -repeat -forever -clip 900x1600+2560+180 &
+fi
