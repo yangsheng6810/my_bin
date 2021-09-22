22bc4fc0b801b545dd473efc267afc4202842b4c
Sheng Yang <yangsheng6810@gmail.com>
2021-09-22 01:50:23 -0500

    Fix switch external screen

--- switch_external_screen.sh
+++ switch_external_screen.sh
@@ -5,12 +5,12 @@ echo $NUM_MONITOR
 
 if [[ "$NUM_MONITOR" == 2 ]]; then
     ssh carbon 'DISPLAY=":0" xrandr --output eDP1 --scale 1x1 --mode 2560x1440 --rotate normal'
-    ssh carbon 'DISPLAY=":0" xset -dpms s off'
+    ssh carbon 'DISPLAY=":0" xset +dpms s 1200 1200'
     killall x11vnc
     xrandr --output HDMI-1 --off --output HDMI-0 --mode "2560x1440" --pos 0x0
 else
     ssh carbon 'DISPLAY=":0" xrandr --output eDP1 --scale 1x1 --mode 1600x900 --rotate right'
-    ssh carbon 'DISPLAY=":0" xset +dpms s 1200 1200'
+    ssh carbon 'DISPLAY=":0" xset -dpms s off'
     xrandr --output HDMI-1 --mode "1600x900" --pos 2560x180 --rotate left --output HDMI-0 --mode "2560x1440" --pos 0x0
     x11vnc -usepw -nocursorshape -nocursorpos -nobell -noxinerama -solid -repeat -forever -clip 900x1600+2560+180 &
 fi
